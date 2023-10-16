d3e83938b561d11b38c6c320f8043b758f8df22e
Gnevilkoko <danik.apelsin@gmail.com>
2023-10-16 17:58:07 +0200

    Added calculation Get data from text boxes and calculate

--- lab3/View/SpirytusForm.cs
+++ lab3/View/SpirytusForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using lab3.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,38 @@ namespace lab3
 {
     public partial class SpirytusForm : Form
     {
+        private ViewModel _viewModel;
         public SpirytusForm()
         {
             InitializeComponent();
+            _viewModel = new ViewModel();
         }
 
         private void goButton_Click(object sender, EventArgs e)
         {
             this.Size = new System.Drawing.Size(610, Height);
+
+            ResultData result = _viewModel.ClickGO(this.dishSizeBox.Text, this.spirytusBox.Text, this.dishesQuantityBox.Text);
+
+            if(result == null)
+            {
+                MessageBox.Show(
+                    "Please, write correct data",
+                    "Invalid Data",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                return;
+            }
+
+            showResult(result);
+
+        }
+
+        private void showResult(ResultData result)
+        {
+            resultSizeBox.Text = result.ResultSize.ToString();
+            spirytusSizeBox.Text = result.SpirytusSize.ToString();
         }
     }
 }
--- lab3/ViewModel/Enums/Dishes.cs
+++ lab3/ViewModel/Enums/Dishes.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lab3.Model
+namespace lab3
 {
     enum Dishes
     {
